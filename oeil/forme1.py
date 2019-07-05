d959f69b65ffce6cc00d073f31686c67958965dc
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-05 23:26:34 +0200

    Update forme1.py
    ok enfete ca prend TOUT le disque... et ca dis quya du blanc alors que c du fond noir et y'a du 111 alors qu'on a dit 000 grrrrr

--- oeil/forme1.py
+++ oeil/forme1.py
@@ -1,9 +1,8 @@
 import cv2
-from colour import *
 import numpy as np
 font = cv2.FONT_HERSHEY_COMPLEX
 
-from couleur.coul import *
+
 
 
 
@@ -11,33 +10,96 @@ PATH = r'C:\Users\jeanbaptiste\Desktop\onebyone\couleur\bobo.txt'
 
 
 def lecture(image):
+    
     im = cv2.imread(image, 0)
-    th = cv2.adaptiveThreshold(im,
-                               255,
-                               cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                               cv2.THRESH_BINARY, 115, 1)
+    th = cv2.adaptiveThreshold(im,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY, 115, 1)
     blur = cv2.medianBlur(th,15)
 
-    cv2.imshow('image', blur)
     cv2.imwrite('traitement.jpg', blur)
 
 
 
-def epparpillation(image):
+
+
+def lecture_noir_blanc(image):
+    
+    im = cv2.imread(image, 0)
+    im2 = cv2.imread('redessinage.jpg', 0)
+
+    for x in range(im.shape[0]):
+        for y in range(im.shape[1]):
+            if im[x,y] < 125:
+                im2[x,y] = 255
+            else:
+                im2[x,y] = 0
+                
+    cv2.imwrite('redessinage.jpg', im2)
+
+
+
+
+
+ 
+
+def fonction_cercle(x, x1, y, y1, r, longueur):
+    r = 10
+    
+    pts = ((x - x0)**2) + ((y - y0)**2)
+    
+    
+
+ROND = []
+LISTE_EPAR = []
+def eparpillation(image):
+
+    les_rond = []
+    
+    im = cv2.imread(image)
+
+    longueur = im.shape[0]
+
     for x in range(im.shape[0]):
         for y in range(im.shape[1]):
 
+            r = 5
+            
+            Ocontinuer = True
+            while Ocontinuer:
+                print(r)
+                rond = cv2.circle(im, (20, 20), r, (False), 1)
+                diametre = len(rond)
+                pts = 0
+                les_rond.append([x,y,r])
+                compt = 0
+                for i in rond:
+                    for j in i:
+                        compt += 1
+                        if j[0] == 255 and j[1] == 255 and j[2] == 255:
+                            pts += 1
+                            
+                print(compt, pts)
+                if pts == compt:
+                    Ocontinuer = True
+                    les_rond = []
+                    pts = 0
+                    r += 5
+
+                elif pts != diametre:
+                    ROND.append(les_rond)
+                    Ocontinuer = False
+
+  
+                cv2.imshow('image', im)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
+                
 
 
 
 
 
 
-def couleur():
-    
-    colordb = get_colordb(PATH)
-    nearest = colordb.nearest(47, 79, 79)
-    print(nearest)
+        
 
 
 
@@ -51,10 +113,12 @@ def couleur():
 
 if __name__ == '__main__':
 
-    PATH_OEIL = r"C:\Users\jeanbaptiste\Desktop\onebyone\oeil\oeil3.jpg"
+    PATH_OEIL = r"C:\Users\jeanbaptiste\Desktop\onebyone\oeil\oeil1.jpg"
 
     lecture(PATH_OEIL)
-    
+    lecture_noir_blanc('traitement.jpg')
+    liste, longueur = eparpillation('redessinage.jpg')
+    cherche(liste, longueur)
 
 
 
